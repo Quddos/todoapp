37d0af9043b6dd310313a0e353a6aa37c5d75378
Bdnature <84246065+Quddos@users.noreply.github.com>
2023-08-29 03:14:10 -0700

    routing in laravel

---
+++ resources/views/index.blade.php
@@ -0,0 +1,2 @@
+<a href="/todoapp">Go to To-do page</a>
+<a href="/contact">Connect with us</a>
---
+++ resources/views/todoapp.blade.php
@@ -0,0 +1,10 @@
+<h1>TO-DO LIST</h1>
+
+<a href="/">Home</a>
+
+<form>
+    <div>
+        <input type="text" placeholder="Enter to-do item" required>
+        <input type="submit" value="Add">
+    </div>
+</form>
